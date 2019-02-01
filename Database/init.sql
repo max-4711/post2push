7c4281ef42fbedfdcff4b9a5f561834147487921
Max Kremer <max.kremer@outlook.com>
2019-02-01 20:06:26 +0100

    ADD: pipeline.yaml, CHG: .gitignore

---
+++ Database/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `channel` 
+    ( `name`                VARCHAR(100)    NOT NULL COMMENT 'Name des Channels, der ihn zugleich eindeutig identifiziert', 
+      `push_secret`         CHAR(50)        NOT NULL COMMENT 'Secret, welches benötigt wird, um in diesen Channel zu pushen',
+      `subscription_secret` CHAR(40)        NOT NULL COMMENT 'Secret, welches benötigt wird, um diesen Channel zu abonnieren', 
+      `creation_timestamp`  TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Zeitpunkt, an dem dieser Channel erstellt wurde',
+      `last_push_timestamp` TIMESTAMP       NULL     DEFAULT NULL COMMENT 'Zeitpunkt, an dem zuletzt etwas in diesen Channel gepushed wurde',
+       PRIMARY KEY (`name`)
+    ) ENGINE = InnoDB COMMENT = 'Enthält sämtliche Push-Channels';
+
+CREATE TABLE `subscription` 
+    ( `token`               CHAR(45)        NOT NULL COMMENT 'Einzigartiges Subscription-Token',
+      `channel_name`        VARCHAR(100)    NOT NULL COMMENT 'Channe-Name, auf den sich diese Subscription bezieht',
+      `push_token`          TEXT            NOT NULL COMMENT 'Token für GCM/Firebase oder Whatever, um Push-Benachrichtigungen an einen Client zu senden',
+      `creation_timestamp`  TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Zeitpunkt, an dem diese Subscription erstellt wurde', 
+      PRIMARY KEY (`token`)
+    ) ENGINE = InnoDB COMMENT = 'Enthält sämtliche Subscriptions';
+
+ALTER TABLE `subscription` 
+    ADD CONSTRAINT `fk_subscription_channel_name` 
+    FOREIGN KEY (`channel_name`) REFERENCES `channel`(`name`) ON DELETE CASCADE ON UPDATE CASCADE;
