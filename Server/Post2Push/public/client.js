0f16a4c4f70e4012de07db7bdfa99da8bb31633c
Max Kremer <max.kremer@outlook.com>
2019-02-02 20:26:29 +0100

    Fix client again + pipeline

--- Server/Post2Push/public/client.js
+++ Server/Post2Push/public/client.js
@@ -9,7 +9,7 @@ if ('serviceWorker' in navigator) {
 async function run() {
     console.log('Registering service worker');
     const registration = await navigator.serviceWorker.
-        register('/worker.js', { scope: '/' });
+        register('https://PIPELINE_INSERT_APP_URL/public/worker.js', { scope: '/' });
     console.log('Registered service worker');
 
     console.log('Registering push');
