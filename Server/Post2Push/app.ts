1962fe925769f6faf6ef236ffd47ef2e6b377dca
Max Kremer <max.kremer@outlook.com>
2019-02-02 16:06:35 +0100

    chg bla

--- Server/Post2Push/app.ts
+++ Server/Post2Push/app.ts
@@ -34,10 +34,14 @@ app.use(function (req, res, next) {
 // development error handler: stacktraces
 if (app.get('env') === 'development') {
     app.use((err: any, req, res, next) => {
-        res.status(err['status'] || 500).send('error', {
+        res.status(400).send('error', {
             message: err.message,
             error: err
         });
+        //res.status(err['status'] || 500).send('error', {
+        //    message: err.message,
+        //    error: err
+        //});
     });
 }
 
