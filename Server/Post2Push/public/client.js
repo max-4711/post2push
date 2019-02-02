6ab9e81f0693490e96d53d0659a289bafc51faa3
Max Kremer <max.kremer@outlook.com>
2019-02-02 11:58:51 +0100

    CHG: Client-JS-Skripte nicht mehr ausgeschlossen

---
+++ Server/Post2Push/public/client.js
@@ -0,0 +1,40 @@
+﻿const publicVapidKey = 'PIPELINE_INSERT_PUBLICVAPIDKEY';
+
+if ('serviceWorker' in navigator) {
+    console.log('Registering service worker');
+
+    run().catch(error => console.error(error));
+}
+
+async function run() {
+    console.log('Registering service worker');
+    const registration = await navigator.serviceWorker.
+        register('/worker.js', { scope: '/' });
+    console.log('Registered service worker');
+
+    console.log('Registering push');
+    var deliveryDetails = await registration.pushManager.
+        subscribe({
+            userVisibleOnly: true,
+            // The `urlBase64ToUint8Array()` function is the same as in
+            // https://www.npmjs.com/package/web-push#using-vapid-key-for-applicationserverkey
+            applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+        });
+    console.log('Registered push');
+
+    var subscription = {
+        ChannelName: 'TestChannel',
+        DeliveryDetails: deliveryDetails,
+        ChannelSubscriptionSecret: 'TestChannelSecret'
+    };
+
+    console.log('Sending push');
+    await fetch('/subscribe', {
+        method: 'POST',
+        body: JSON.stringify(subscription),
+        headers: {
+            'content-type': 'application/json'
+        }
+    });
+    console.log('Sent push');
+}
---
+++ Server/Post2Push/public/worker.js
@@ -0,0 +1,10 @@
+﻿console.log('Loaded service worker!');
+
+self.addEventListener('push', ev => {
+    const data = ev.data.json();
+    console.log('Got push', data);
+    self.registration.showNotification(data.title, {
+        body: 'Benachrichtigungen aktiviert.',
+        icon: 'https://PIPELINE_INSERT_DOMAINNAME/public/success.png'
+    });
+});
